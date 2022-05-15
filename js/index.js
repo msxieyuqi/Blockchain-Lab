854e4552472b8c362954b72540bf3ae97db29e0c
Yuqi <asd_1031@163.com>
2022-05-16 03:46:10 +0800

    jquery.js/index.js(select chain A/B function); change "recevied->recevier address"

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
 $(function() {
+    selectChainA();
+    selectChainB();
+    approval();
+});
 
-})
+// chainA 下拉选择框
+function selectChainA() {
+    // _selected保存选择内容
+    var _selected;
+    _selected = $('#selectChainABtn').text();
+    $('#selectChainA').on('click', '.chainA', function() {
+        $(this).parent().siblings('button').text($(this).text());
+        _selected = $(this).parent().siblings('button').text();
+        console.log(_selected);
+    });
+}
+
+// chainB 下拉选择框
+function selectChainB() {
+    // _selected保存选择内容
+    var _selected;
+    _selected = $('#selectChainBBtn').text();
+    $('#selectChainB').on('click', '.chainB', function() {
+        $(this).parent().siblings('button').text($(this).text());
+        _selected = $(this).parent().siblings('button').text();
+        console.log(_selected);
+    });
+}
+
+//approval 按钮
+function approval() {
+    $('#approval').on('click', function() {
+        alert('approval');
+    });
+}
