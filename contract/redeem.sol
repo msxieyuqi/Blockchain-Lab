3a80fa31b2031dc5a9f0529cf07a003ea8d40323
Angelafat <105603708+Angelafat@users.noreply.github.com>
2022-05-20 23:03:22 +0200

    Update redeem.sol

--- contract/redeem.sol
+++ contract/redeem.sol
@@ -2,7 +2,7 @@ pragma solidity 0.6.0;
 import "./hashtimelock.sol";
 contract Redeem is HashedTimeLock {
 
-    function redeem(bytes32 _contractId, bytes32 _key) public payable returns(bool) {
+    function redeem(bytes32 _contractId, bytes32 _key) external payable returns(bool) {
         withdraw(_contractId, _key);
     }
 }   
